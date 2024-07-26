c3ebea29d0360e6095c923f4d1879b35143e13c1
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-07-26 15:11:19 +0200

    impostazioni per l'immagine

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,63 @@
 import multimediaAudioeVideo.Audio;
 import multimediaAudioeVideo.Video;
+import multimediaImmagine.Immagine;
 
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Desideri un audio o un video? (Scrivi 'audio' o 'video'):");
+        System.out.println("Desideri un audio o un video? (Scrivi 'audio', 'video' o 'immagine' ):");
         String type = scanner.nextLine();
 
         System.out.println("Per favore scrivi il titolo:");
-        String audioTitle = scanner.nextLine();
+        String title = scanner.nextLine();
+
+        if (type.equalsIgnoreCase("audio") || type.equalsIgnoreCase("video")) {
+            System.out.println("Per favore scrivi la durata:");
+            int duration = Integer.parseInt(scanner.nextLine());
 
 
-        System.out.println("Per favore scrivi la durata:");
-        int audioDuration = Integer.parseInt(scanner.nextLine());
+            System.out.println("Per favore scegli il volume (0-5):");
+            int volume = Integer.parseInt(scanner.nextLine());
 
+            if (type.equalsIgnoreCase("video")) {
+                System.out.println("Per favore scegli la luminosità (0-5):");
+                int brightness = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Per favore scegli il volume (0-5):");
-        int audioVolume = Integer.parseInt(scanner.nextLine());
+                // Creo un'istanza di Video
 
-        // Eseguo un if che in base alla scelta tra video o audio mi stampi le funzionalità richieste
-        if (type.equalsIgnoreCase("video")) {
+                Video newVideo = new Video(title, duration);
+                newVideo.setVolume(volume);
+                newVideo.setBrightness(brightness);
+
+                newVideo.play();
+            } else {
+
+                // Creo un'istanza di Audio
+
+                Audio newAudio = new Audio(title, duration);
+                newAudio.setVolume(volume);
+
+                newAudio.play();
+            }
+        } else if (type.equalsIgnoreCase("immagine")) {
 
 
             System.out.println("Per favore scegli la luminosità (0-5):");
             int brightness = Integer.parseInt(scanner.nextLine());
 
-            // Creo un'istanza di Video
-            Video newVideo = new Video(audioTitle, audioDuration);
-            newVideo.setVolume(audioVolume);
-            newVideo.setBrightness(brightness);
+            // Creo un'istanza di Immagine
 
-            newVideo.play();
+            Immagine newImmagine = new Immagine(title);
+            newImmagine.setBrightness(brightness);
 
+            newImmagine.view();
         } else {
-            // Creo un'istanza di Audio
-            Audio newAudio = new Audio(audioTitle, audioDuration);
-            newAudio.setVolume(audioVolume);
-
-
-            newAudio.play();
+            System.out.println("Tipo di media non valido. Per favore, riavvia il programma e inserisci 'audio', 'video', or 'immagine'.");
         }
-
         scanner.close();
     }
 }
--- src/multimediaImmagine/Immagine.java
+++ src/multimediaImmagine/Immagine.java
@@ -1,5 +1,33 @@
 package multimediaImmagine;
 
 public class Immagine {
+    protected String mediaTitle;
+    private int brightness = 2; // Luminosità di default
 
+    public Immagine(String mediaTitle) {
+        this.mediaTitle = mediaTitle;
+    }
+
+    public void view() {
+        System.out.println(this.mediaTitle + "*".repeat(this.brightness));
+    }
+
+    public void raiseBrightness() {
+        if (brightness < 5) {
+            this.brightness += 1;
+        }
+    }
+
+    public void lowerBrightness() {
+        if (brightness > 0) {
+            this.brightness -= 1;
+        }
+    }
+
+    // Metodo setter per la luminosità
+    public void setBrightness(int brightness) {
+        if (brightness >= 0 && brightness <= 5) {
+            this.brightness = brightness;
+        }
+    }
 }
