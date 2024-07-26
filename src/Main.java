e4252490ebf1b7ab424295bcc411de7823033557
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-07-26 14:31:30 +0200

    impostazioni per video

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import multimediaAudioeVideo.Audio;
+import multimediaAudioeVideo.Video;
 
 import java.util.Scanner;
 
@@ -8,24 +9,42 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Per favore scrivi il testo della canzone:");
+        System.out.println("Desideri un audio o un video? (Scrivi 'audio' o 'video'):");
+        String type = scanner.nextLine();
+
+        System.out.println("Per favore scrivi il titolo:");
         String audioTitle = scanner.nextLine();
 
 
-        System.out.println("Per favore scrivi la durata della canzone:");
+        System.out.println("Per favore scrivi la durata:");
         int audioDuration = Integer.parseInt(scanner.nextLine());
 
 
         System.out.println("Per favore scegli il volume (0-5):");
         int audioVolume = Integer.parseInt(scanner.nextLine());
 
+        // Eseguo un if che in base alla scelta tra video o audio mi stampi le funzionalità richieste
+        if (type.equalsIgnoreCase("video")) {
 
-        Audio newAudio = new Audio(audioTitle, audioDuration);
-        newAudio.setVolume(audioVolume);
+
+            System.out.println("Per favore scegli la luminosità (0-5):");
+            int brightness = Integer.parseInt(scanner.nextLine());
+
+            // Creo un'istanza di Video
+            Video newVideo = new Video(audioTitle, audioDuration);
+            newVideo.setVolume(audioVolume);
+            newVideo.setBrightness(brightness);
+
+            newVideo.play();
+
+        } else {
+            // Creo un'istanza di Audio
+            Audio newAudio = new Audio(audioTitle, audioDuration);
+            newAudio.setVolume(audioVolume);
 
 
-        newAudio.play();
-
+            newAudio.play();
+        }
 
         scanner.close();
     }
--- src/multimediaAudioeVideo/Video.java
+++ src/multimediaAudioeVideo/Video.java
@@ -1,4 +1,51 @@
 package multimediaAudioeVideo;
 
-public class Video {
+public class Video extends Audio {
+    private int brightness = 2; // Imposto una luminosità di default
+
+    public Video(String mediaTitle, int duration) {
+        super(mediaTitle, duration);
+    }
+
+    // Metodo per stampare il video con la luminosità
+
+    @Override
+    public void play() {
+        int printCount = duration + volume + brightness;
+        String puntoEsclamativo = "!".repeat(volume);
+        String asterisco = "*".repeat(brightness);
+
+        //ciclo for che in base al volume, luminosità e durata stampi il titolo tot num di volte
+        
+        for (int i = 0; i < printCount; i++) {
+            System.out.println(this.mediaTitle + puntoEsclamativo + asterisco);
+        }
+    }
+
+    // Metodo raiseBrightness() per aumentare la luminosità
+
+    public void raiseBrightness() {
+        if (brightness < 5) {
+            this.brightness += 1;
+        }
+    }
+
+    // Metodo lowerBrightness() per diminuire la luminosità
+
+    public void lowerBrightness() {
+        if (brightness > 0) {
+            this.brightness -= 1;
+        }
+    }
+
+    // Metodo setter per la luminosità
+
+
+    public void setBrightness(int brightness) {
+        if (brightness >= 0 && brightness <= 5) {
+            this.brightness = brightness;
+        }
+    }
 }
+
+
